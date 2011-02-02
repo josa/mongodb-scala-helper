a8ca11681b7e10005102fb01d0231c076459d108
Jeosadache Galvão <josa.galvao@gmail.com>
2011-02-02 01:45:05 -0200

    feito um pequeno refactory

--- src/test/scala/br/com/gfuture/mongodbhelper/QuerySpec.scala
+++ src/test/scala/br/com/gfuture/mongodbhelper/QuerySpec.scala
@@ -5,20 +5,26 @@ import org.scalatest.{Spec, BeforeAndAfterEach}
 
 class QuerySpec extends Spec with ShouldMatchers with BeforeAndAfterEach {
 
-  var entity:EntityTest = null
+  var entity: EntityTest = null
 
-  override def beforeEach(){
+  override def beforeEach() {
     entity = new EntityTest
     entity.title = "Entidade para pesquisa"
     entity.save
   }
 
-  describe("br.com.gfuture.mongodbhelper.Query"){
+  describe("br.com.gfuture.mongodbhelper.Query") {
 
-    it("should find by id"){
+    it("should find by id") {
       val query = new Query[EntityTest](classOf[EntityTest]);
       val entityFind = query.findById(entity.getObjectId)
       entityFind should equal(entity)
+    }
+
+    it("should return null in false id") {
+      val query = new Query[EntityTest](classOf[EntityTest]);
+      val entityFind = query.findById(org.bson.types.ObjectId.get)
+      entityFind should equal(null)
     }
 
   }
